749c81a9bf9b0fe94319383701ed38e3770bae49
tanuj <tanujbhandari10@gmail.com>
2025-06-14 15:30:42 +0530

    hii

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -10,7 +10,7 @@ const Register = () => {
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
-      await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/register`, { email, password });
+      await axios.post("https://spendsmart-expensetracker.onrender.com/api/auth/register", { email, password });
       navigate("/login");
     } catch (err) {
       alert("Something went wrong! Try a different email.");
