74742c61c33944cb0145e4b8e2fd74583884d122
tanuj <tanujbhandari10@gmail.com>
2025-06-14 13:34:19 +0530

    production ready

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const authRoutes = require("./routes/auth");
 const transactionRoutes = require("./routes/transactions");
 
 const app = express();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://spend-smart-expense-tracker-six.vercel.app", credentials: true }));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
