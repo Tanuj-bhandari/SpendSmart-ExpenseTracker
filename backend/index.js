487595b7ce275657a18d6879e0c815c9a4ddb10d
tanuj <tanujbhandari10@gmail.com>
2025-06-14 14:56:34 +0530

    final commit

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
-const PORT = process.env.PORT ||5000;
+const PORT = process.env.PORT || 5000;
 
 const authRoutes = require("./routes/auth");
 const transactionRoutes = require("./routes/transactions");
@@ -15,6 +15,5 @@ app.use("/api/auth", authRoutes);
 app.use("/api/transactions", transactionRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => app.listen(PORT, () => console.log("Server running on port 5000")))
+  .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))  // use PORT here
   .catch((err) => console.error(err));
-
