d6b7e1c5034298c5d5a901c7b422b220fe95f968
tanuj <tanujbhandari10@gmail.com>
2025-06-14 14:01:37 +0530

    minor change

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 
 const authRoutes = require("./routes/auth");
 const transactionRoutes = require("./routes/transactions");
